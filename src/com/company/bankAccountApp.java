b64a752f231d1dbda8a0589fb40b8fef3829b766
DimitrisKapoulas <kapoulasd@gmail.com>
2019-08-30 00:12:52 +0300

    Adding transactions methods

--- src/com/company/bankAccountApp.java
+++ src/com/company/bankAccountApp.java
@@ -8,7 +8,9 @@ public class bankAccountApp {
         Checking chk1 = new Checking("Nelson Oliveira", "987456321", 2500);
         sav1.showInfo();
         chk1.showInfo();
-	// Read a CSV file, then create new accounts based on that date
-
+        sav1.deposit(5000);
+        chk1.withdraw(4020);
+        sav1.transfer("Brokerage", 500);
+        // Read a CSV file, then create new accounts based on that date
     }
 }
